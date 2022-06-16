11dd644f57500476e02e9bdb159c0ac75e515c9d
Suresh17 <suresh.20fk@gmail.com>
2022-06-16 08:59:35 +0530

    working on js part of the login page

--- expedia_login.js
+++ expedia_login.js
@@ -1,2 +1,32 @@
 
+let form = document.querySelector("form")
 
+let user_arr = JSON.parse(localStorage.getItem("userData")) || [];
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+
+    let userObj = {
+        user_email : form.email.value,
+        user_password : form.password.value,
+    }
+    if (check_credential(userObj.user_email,userObj.user_password)==true){
+        localStorage.setItem("login",JSON.stringify(userObj));
+        alert("login successful !")
+    }else {
+        alert("invalid email id or password !")
+    }
+
+})
+
+function check_credential (user_email,user_password){
+    let filtered = user_arr.filter(function(elem){
+        return elem.user_email === user_email && elem.user_password === user_password;
+    })
+    
+    if (filtered.length > 0){
+        return true;
+    }else {
+        return false;
+    }
+}
